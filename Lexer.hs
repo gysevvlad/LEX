f767f1c53d286c8544f85e905bebb5b395b782ed
gysevvlad <gysevvlad@gmail.com>
2015-02-24 17:37:35 +0300

    found bug
    need "number = float | int" because float includes int

--- Lexer.hs
+++ Lexer.hs
@@ -53,62 +53,9 @@ checkDef gr (ch, str) (def) = foldl (checkPr gr) (ch, str) def
 
 checkPr :: Grammar -> (Bool, String) -> String -> (Bool, String)
 checkPr gr st@(ch, str) pr
-	| ch == False = st
-	| ch == True = is gr st pr
-{-
-createAST :: Grammar -> (Bool, String) -> String -> (String, Maybe AST)
-createAST gr (ch, str) pr
-	| l == '\'' && r == '\'' = 
-		if base `isPrefixOf` str
-		then (tail str, Just $ Node base [])
-		else (str, Nothing)
-	| otherwise = 
-		case find ( (==) pr . fst) gr of
-			Just (_, defs) -> createkDefs gr (ch, str) defs
-			Nothing -> error "unknown productions"
-	where 
-		l = head pr
-		r = last pr
-		base = (tail . init) pr
+	| ch == False 	= st
+	| ch == True 	= is gr st pr
 
-checkDefs :: Grammar -> (Bool, String) -> [[String]] -> (Bool, String)
-checkDefs gr (ch, str) [] = (False, str)
-checkDefs gr (ch, str) (def:defs) = let st@(ch', str') = checkDef gr (ch, str) def in
-	if (ch' == True)
-	then st
-	else checkDefs gr (ch, str) defs
-
-checkDef :: Grammar -> (Bool, String) -> [String] -> (Bool, String)
-checkDef gr (ch, str) (def) = foldl (checkPr gr) (ch, str) def
-
-createDef :: Grammar -> [String] -> String -> (String, Maybe AST)
-createDef gr def@(pr:prs) str = 
--}
-{-
-createPr :: Grammar -> String -> String -> (String, Maybe AST)
-createPr gr pr str
-	| l == '\'' && r == '\'' = 
-		if base `isPrefixOf` str
-		then (tail str, Just $ Node base [])
-		else (str, Nothing)
-	| otherwise =
-		case find ( (==) pr . fst) gr of
-			Just (_, defs) -> 
-				let (str', ast) = createkDefs gr defs str in
-
-					then (str, Nothing)
-					else (str', Just $ Node pr asts)
-			Nothing -> (str, Nothing)
-	where
-		l = head pr
-		r = last pr
-		base = (tail . init) pr
-		
-createkDefs :: Grammar -> [[String]] -> String -> (String, Maybe AST)
-createkDefs gr defs str 
-
-test gr = createPr (readGrammar gr)
--}
 sup1 =  concatMap forElem .grammarToForest .readGrammar
 	where forElem = (++) "\n" .drawTree .mapForDT show .snd .toNumTree .(\x -> (1, x))
 
